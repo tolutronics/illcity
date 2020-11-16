4e154a2662ed247004c9532430500d267089b50b
tolutronics <toluxy100@gmail.com>
2020-11-16 10:51:54 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: true,
     firebase: {
-      apiKey: "AIzaSyBGuqMaKLeoX2tJk3uuaaQ_9OLuHpUWoyA",
-      authDomain: "ilcity-b6316.firebaseapp.com",
-      databaseURL: "https://ilcity-b6316.firebaseio.com",
-      projectId: "ilcity-b6316",
-      storageBucket: "ilcity-b6316.appspot.com",
-      messagingSenderId: "406860301906",
+  
   }
 };
 
